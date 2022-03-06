f541a7815206e3509649c70edcd32052a3b72a7f
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-03-06 13:33:17 -0500

    webpack done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//console.log('hello world') //this code we should see in the browser
+
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<hr />, document.querySelector('#root'))
